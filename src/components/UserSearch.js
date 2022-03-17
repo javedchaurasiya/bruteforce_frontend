6c9484692706535adc2ea7357e75fd02284a238d
Ritu Raj Shandilya <riturajyo147147@gmail.com>
2022-03-17 12:08:29 +0530

    search user page complete

--- src/components/UserSearch.js
+++ src/components/UserSearch.js
@@ -3,54 +3,47 @@ import { Link } from "react-router-dom";
 import { Avatar } from "@mui/material";
 import "./userSearch.css";
 
-const stringToColor = (string) => {
-  let hash = 0;
-  let i;
+// const stringToColor = (string) => {
+//   let hash = 0;
+//   let i;
 
-  /* eslint-disable no-bitwise */
-  for (i = 0; i < string.length; i += 1) {
-    hash = string.charCodeAt(i) + ((hash << 5) - hash);
-  }
+//   /* eslint-disable no-bitwise */
+//   for (i = 0; i < string.length; i += 1) {
+//     hash = string.charCodeAt(i) + ((hash << 5) - hash);
+//   }
 
-  let color = "#";
+//   let color = "#";
 
-  for (i = 0; i < 3; i += 1) {
-    const value = (hash >> (i * 8)) & 0xff;
-    color += `00${value.toString(16)}`.substr(-2);
-  }
-  /* eslint-enable no-bitwise */
+//   for (i = 0; i < 3; i += 1) {
+//     const value = (hash >> (i * 8)) & 0xff;
+//     color += `00${value.toString(16)}`.substr(-2);
+//   }
+//   /* eslint-enable no-bitwise */
 
-  return color;
-};
+//   return color;
+// };
 
-const stringAvatar = (name) => {
-  return {
-    sx: {
-      bgcolor: stringToColor(name),
-    },
-    children: `${name.split(" ")[0][0]}${name.split(" ")[1][0]}`,
-  };
-};
+// const stringAvatar = (name) => {
+//   return {
+//     sx: {
+//       bgcolor: stringToColor(name),
+//     },
+//     children: `${name.split(" ")[0][0]}${name.split(" ")[1][0]}`,
+//   };
+// };
 
-function UserSearch({
-  name = "Def name",
-  imageURL = "",
-  questions = 100,
-  userName = "",
-}) {
-  const x = name;
-  console.log(x);
+function UserSearch(props) {
+  const {user}=props
+  // console.log(user);
   return (
-    <Link to={"/" + userName}>
+    <Link to={"/" + user.user_name}>
       <div className="search-item">
         <div className="avatar-container">
-          <Avatar src={imageURL} {...stringAvatar(name)} />
+          <Avatar src={user.imageURL} />
         </div>
         <div className="details-container">
-          <div className="name-field">
-            {name}
-          </div>
-          <div className="solved">{questions} questions solved</div>
+          <div className="name-field">{user.name}</div>
+          <div className="solved">{user.location}</div>
         </div>
       </div>
     </Link>
--- src/pages/SearchUser.js
+++ src/pages/SearchUser.js
@@ -1,23 +1,51 @@
-import React from "react";
+import { React, useState, useEffect } from "react";
 import Container from "@mui/material/Container";
 import TextField from "@mui/material/TextField";
 import "./searchUser.css";
 import UserSearch from "../components/UserSearch";
 import { InputAdornment } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
+import axios from "axios";
 
 function SearchUser() {
+  const serverURL = "http://localhost:2000/";
+
+  const [query, setQuery] = useState("");
+  const [result, setResult] = useState([]);
+
+  useEffect(() => {
+    const getUsers = setTimeout(async () => {
+      try {
+        const response = await axios.post(serverURL + "search", {
+          name: query,
+        });
+        // console.log(response.data);
+        if (query !== "") setResult(response.data.result);
+        else setResult([])
+      } catch (error) {
+        console.log(error);
+        alert("Something Went wrong");
+      }
+    }, 1500);
+
+    return () => clearTimeout(getUsers);
+  }, [query]);
+
   return (
     <div className="search-body">
-      <Container fixes sx={{ background: "#f5f5f5", minWidth: "500px" }}>
+      <Container fixed sx={{ background: "#f5f5f5", minWidth: "500px" }}>
         <div className="items">
           <div className="search-box">
             <TextField
               fullWidth
+              value={query}
               id="Search User"
               label="Search"
               variant="outlined"
               placeholder="Search User..."
+              onChange={(e) => {
+                setQuery(e.target.value);
+              }}
               InputProps={{
                 startAdornment: (
                   <InputAdornment position="start">
@@ -28,12 +56,9 @@ function SearchUser() {
             />
           </div>
           <div className="search-results">
-            <UserSearch name="user one" userName="bajak" questions={120} />
-            <UserSearch name="user two" userName="raja" questions={150} />
-            <UserSearch name="user three" userName="rahul" questions={200} />
-            <UserSearch name="user one" userName="bajak" questions={120} />
-            <UserSearch name="user two" userName="raja" questions={150} />
-            <UserSearch name="user three" userName="rahul" questions={200} />
+            {result.map((user) => (
+              <UserSearch key={user.user_name} user={user} />
+            ))}
           </div>
         </div>
       </Container>
